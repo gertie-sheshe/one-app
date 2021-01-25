dd07ec52eb64c9907069c2914566d4994748d19d
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-01-25 20:46:57 +0300

    log

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('TEST', process.env.REACT_APP_NAME)
   return (
     <div className="App">
       <p>One Two Three</p>
