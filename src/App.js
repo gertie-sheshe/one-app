0757b46abf5e224a77485435a60629fa988bc1c7
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-01-26 09:28:08 +0300

    update yaml facepalm

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 
 function App() {
   console.log('TEST', process.env.REACT_APP_NAME)
-  console.log('TOKEN', process.env.REACT_APP_NAME_ONE)
-  console.log('API', process.env.REACT_APP_NAME_TWO)
+  console.log('TOKEN', process.env.REACT_APP_ACCESS_TOKEN)
+  console.log('API', process.env.REACT_APP_API)
   return (
     <div className="App">
       <p>One Two Three</p>
       <p>{process.env.REACT_APP_NAME}</p>
       <p>Try again and again</p>
-  <p>Access Token {process.env.REACT_APP_NAME_ONE}</p>
-  <p>API KEY {process.env.REACT_APP_NAME_TWO}</p>
+  <p>Access Token {process.env.REACT_APP_ACCESS_TOKEN}</p>
+  <p>API KEY {process.env.REACT_APP_API}</p>
   <p>HUUUUH</p>
     </div>
   );
