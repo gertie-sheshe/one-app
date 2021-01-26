542c95a7aab7988b8a6691b47b0767ff85bf986b
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-01-26 09:10:08 +0300

    one more

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 function App() {
   console.log('TEST', process.env.REACT_APP_NAME)
   console.log('TOKEN', process.env.REACT_APP_ACCESS_TOKEN)
+  console.log('API', process.env.REACT_APP_API)
   return (
     <div className="App">
       <p>One Two Three</p>
       <p>{process.env.REACT_APP_NAME}</p>
       <p>Try again and again</p>
   <p>Access Token {process.env.REACT_APP_ACCESS_TOKEN}</p>
+  <p>API KEY {process.env.REACT_APP_API}</p>
     </div>
   );
 }
